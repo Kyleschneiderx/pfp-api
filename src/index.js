180a842e2160d84564b00f51de336ad00feb66f2
Allen Poblete <allen.poblete@guerilla360.com>
2024-08-30 16:01:28 +0800

    trying true value for origin for now to use the request header origin

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const app = express();
 
 app.use(
     cors({
-        origin: ['http://localhost'],
+        origin: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE'],
         optionsSuccessStatus: 200,
     }),
