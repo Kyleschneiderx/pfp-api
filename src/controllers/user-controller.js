aa8c9d096ef3db88131dd3d863efde75d0ccae0f
Allen Poblete <allen.poblete@guerilla360.com>
2024-08-30 15:29:08 +0800

    transfer default declaration for page and page items here from user service

--- src/controllers/user-controller.js
+++ src/controllers/user-controller.js
@@ -1,3 +1,5 @@
+import { REPORT_DEFAULT_PAGE, REPORT_DEFAULT_ITEMS } from '../constants/index.js';
+
 export default class UserController {
     constructor({ userService }) {
         this.userService = userService;
@@ -17,14 +19,13 @@ export default class UserController {
     }
 
     async handleGetUsersRoute(req, res) {
-        console.log(req.query);
         const users = await this.userService.getUsers({
             email: req.query.email,
             userId: req.query.user_id,
             name: req.query.name,
             sort: req.query.sort,
-            page: req.query.page,
-            page_items: req.query.page_items,
+            page: req.query.page ?? REPORT_DEFAULT_PAGE,
+            page_items: req.query.page_items ?? REPORT_DEFAULT_ITEMS,
         });
 
         return res.json(users);
