ea6881209c1cd9966f6930a8ef1b40cd1593c03e
Allen Poblete <allen.poblete@guerilla360.com>
2024-09-02 15:37:05 +0800

    added common validation for user id

---
+++ src/middlewares/validations/common/user-id.js
@@ -0,0 +1,13 @@
+import { param } from 'express-validator';
+
+export default ({ userService }) => [
+    param('user_id')
+        .exists({ values: 'falsy' })
+        .withMessage('User id is required.')
+        .customSanitizer((value) => Number(value))
+        .custom(async (value) => {
+            if (!(await userService.isUserExistByUserId(value))) {
+                throw new Error('User does not exist.');
+            }
+        }),
+];
