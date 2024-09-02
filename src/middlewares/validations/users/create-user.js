a6942dde9173ebd25cf49adfbc706624d4044600
Allen Poblete <allen.poblete@guerilla360.com>
2024-09-02 13:09:48 +0800

    added optional validation for photo

--- src/middlewares/validations/users/create-user.js
+++ src/middlewares/validations/users/create-user.js
@@ -1,6 +1,7 @@
 import { body } from 'express-validator';
+import * as commonValidation from '../common/index.js';
 
-export default ({ userService }) => [
+export default ({ userService, file }) => [
     body('email')
         .trim()
         .exists({ values: 'falsy' })
@@ -16,4 +17,5 @@ export default ({ userService }) => [
     body('birthdate').trim().exists({ values: 'falsy' }).withMessage('Birthdate is required.').isDate().isISO8601(),
     body('contact_number').trim().exists({ values: 'falsy' }).withMessage('Contact number is required.').isString().isNumeric(),
     body('description').trim().optional().isString(),
+    ...commonValidation.photoValidation({ field: 'photo', file: file }),
 ];
