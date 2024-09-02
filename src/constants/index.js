211d40d81206990fb36ffbadc67b70f71d5eaed3
Allen Poblete <allen.poblete@guerilla360.com>
2024-09-02 13:11:05 +0800

    added temporary value for allowed image and video extension added temporary valud for maximum file size for image and video

--- src/constants/index.js
+++ src/constants/index.js
@@ -11,3 +11,11 @@ export const ACTIVE_STATUS_ID = 1;
 export const REPORT_DEFAULT_ITEMS = 50;
 
 export const REPORT_DEFAULT_PAGE = 1;
+
+export const ALLOWED_PHOTO_TYPE = ['jpg', 'jpeg', 'png'];
+
+export const ALLOWED_VIDEO_TYPE = ['mp4', 'avi', 'mov'];
+
+export const MAX_PHOTO_SIZE_IN_MB = 5;
+
+export const MAX_VIDEO_SIZE_IN_MB = 20;
