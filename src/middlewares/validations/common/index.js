4d15a9431021afa6103427b6fd58d775f5627a17
Allen Poblete <allen.poblete@guerilla360.com>
2024-09-02 15:37:48 +0800

    export user id validation export update user validation

--- src/middlewares/validations/common/index.js
+++ src/middlewares/validations/common/index.js
@@ -1,3 +1,4 @@
 export { default as paginationValidation } from './pagination.js';
 export { default as photoValidation } from './photo.js';
 export { default as videoValidation } from './video.js';
+export { default as userIdValidation } from './user-id.js';
--- src/middlewares/validations/users/index.js
+++ src/middlewares/validations/users/index.js
@@ -1,3 +1,4 @@
 export { default as createUserValidation } from './create-user.js';
 export { default as removeUserValidation } from './remove-user.js';
 export { default as getUsersValidation } from './get-users.js';
+export { default as updateUserValidation } from './update-user.js';
