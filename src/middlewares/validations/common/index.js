03d3b7914abb3ca1691b91a2bea5453730cccc84
Allen Poblete <allen.poblete@guerilla360.com>
2024-09-02 13:09:21 +0800

    added common validation for photo added common validation for video export common validations photo and video

--- src/middlewares/validations/common/index.js
+++ src/middlewares/validations/common/index.js
@@ -1,1 +1,3 @@
 export { default as paginationValidation } from './pagination.js';
+export { default as photoValidation } from './photo.js';
+export { default as videoValidation } from './video.js';
---
+++ src/middlewares/validations/common/photo.js
@@ -0,0 +1,22 @@
+import { check } from 'express-validator';
+import * as constants from '../../../constants/index.js';
+
+export default ({ field, file }) => [
+    check(field).custom((value, { req }) => {
+        if (req.files === undefined) return false;
+
+        const photo = req.files?.[field];
+
+        if (photo !== undefined) {
+            if (photo.mimetype.includes('image') === false) throw new Error('You can only upload image file.');
+
+            if (!constants.ALLOWED_PHOTO_TYPE.includes(file.extractExtension(photo.name))) throw new Error('Invalid image file type.');
+
+            if (!file.isValidFileSize(photo.size, constants.MAX_PHOTO_SIZE_IN_MB)) {
+                throw Error(`Maximum image file size can be upload is ${constants.MAX_PHOTO_SIZE_IN_MB} MB.`);
+            }
+        }
+
+        return true;
+    }),
+];
