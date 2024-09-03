c7cd56a0f5e89927e8b9f270a03a4d5558566189
Allen Poblete <allen.poblete@guerilla360.com>
2024-09-03 11:17:24 +0800

    remove authenticateAccount as the process of authentication is moved to validation middleware added generateSession

--- src/services/auth-service.js
+++ src/services/auth-service.js
@@ -1,53 +1,34 @@
-import * as exceptions from '../exceptions/index.js';
-
 export default class AuthService {
-    constructor({ logger, database, jwt, password, userService }) {
+    constructor({ logger, database, jwt, userService }) {
         this.database = database;
         this.logger = logger;
         this.jwt = jwt;
-        this.password = password;
         this.userService = userService;
     }
 
     /**
-     * Authenticate user account
+     * Generate session for authenticated user
      *
-     * @param {object} data
-     * @param {number} data.accountTypeId User account type id
-     * @param {string=} data.googleId User account google id. Required if user is authenticating using google account
-     * @param {string=} data.appleId User account apple id. Required if user is authenticating using apple account
-     * @param {string=} data.email User account email address
-     * @param {string=} data.password User account password
+     * @param {Users} user User instance
      * @returns {{ user: object, token: { token: string, expires: number }}} Authenticated user object
-     * @throws {UnprocessableContent} If user credential is invalid
      * @throws {InternalServerError} If failed to update user last login time
      * @throws {InternalServerError} If failed to generate JWT token
      */
-    async authenticateAccount(data) {
-        const userInfo = await this.userService.getUser({ email: data.email, accountTypeId: data.accountTypeId });
-
-        if (!userInfo) {
-            throw new exceptions.UnprocessableContent('Incorrect email or password');
-        }
-
-        if (!this.password.verify(data.password, userInfo.password)) {
-            throw new exceptions.UnprocessableContent('Incorrect email or password');
-        }
-
-        delete userInfo.dataValues.password;
-        delete userInfo.dataValues.google_id;
-        delete userInfo.dataValues.apple_id;
+    async generateSession(user) {
+        delete user.dataValues.password;
+        delete user.dataValues.google_id;
+        delete user.dataValues.apple_id;
 
         const token = this.jwt.generate(
             process.env.JWT_SECRET,
-            { user: { user_id: userInfo.id, account_type_id: userInfo.account_type_id } },
+            { user: { user_id: user.id, account_type_id: user.account_type_id } },
             { algorithm: process.env.JWT_ALGORITHM },
         );
 
-        await this.userService.updateUserLastLogin(userInfo.id);
+        await this.userService.updateUserLastLogin(user.id);
 
         return {
-            user: userInfo,
+            user: user,
             token: {
                 access: token.token,
                 expires: token.expires,
