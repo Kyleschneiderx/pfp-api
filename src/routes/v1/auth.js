8787768ab44320589d283d8fac69bd96ffdf03ed
Allen Poblete <allen.poblete@guerilla360.com>
2024-09-03 11:18:05 +0800

    added dependency injection for userService, password and isAdmin for loginValidation

--- src/routes/v1/auth.js
+++ src/routes/v1/auth.js
@@ -2,12 +2,20 @@ import express from 'express';
 import validateInput from '../../middlewares/validate-input.js';
 import * as validations from '../../middlewares/validations/auth/index.js';
 
-export default ({ authController }) => {
+export default ({ authController, userService, password }) => {
     const router = express.Router();
 
-    router.post('/login/admin', validateInput(validations.loginValidation()), authController.handleLoginAdminRoute.bind(authController));
+    router.post(
+        '/login/admin',
+        validateInput(validations.loginValidation({ userService: userService, password: password, isAdmin: true })),
+        authController.handleLoginRoute.bind(authController),
+    );
 
-    router.post('/login/user', validateInput(validations.loginValidation()), authController.handleLoginUserRoute.bind(authController));
+    router.post(
+        '/login/user',
+        validateInput(validations.loginValidation({ userService: userService, password: password, isAdmin: false })),
+        authController.handleLoginRoute.bind(authController),
+    );
 
     return router;
 };
