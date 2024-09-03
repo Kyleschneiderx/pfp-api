03790c532718cc7ec970aba6a36cd7d814dd2f59
Allen Poblete <allen.poblete@guerilla360.com>
2024-09-03 11:12:24 +0800

    merge handleLoginAdminRoute and handleLoginUserRoute into handleLoginRoute as autService will only generateSession now instead of authenticating user

--- src/controllers/auth-controller.js
+++ src/controllers/auth-controller.js
@@ -1,28 +1,10 @@
-import { USER_ACCOUNT_TYPE_ID, ADMIN_ACCOUNT_TYPE_ID } from '../constants/index.js';
-
 export default class AuthController {
     constructor({ authService }) {
         this.authService = authService;
     }
 
-    async handleLoginAdminRoute(req, res) {
-        const authenticate = await this.authService.authenticateAccount({
-            accountTypeId: ADMIN_ACCOUNT_TYPE_ID,
-            email: req.body.email,
-            password: req.body.password,
-        });
-
-        return res.json(authenticate);
-    }
-
-    async handleLoginUserRoute(req, res) {
-        const authenticate = await this.authService.authenticateAccount({
-            accountTypeId: USER_ACCOUNT_TYPE_ID,
-            email: req.body.email,
-            password: req.body.password,
-            googleId: req.body.google_id,
-            appleId: req.body.apple_id,
-        });
+    async handleLoginRoute(req, res) {
+        const authenticate = await this.authService.generateSession(req.user);
 
         return res.json(authenticate);
     }
