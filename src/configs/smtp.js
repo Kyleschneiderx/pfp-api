28b544f5a2d6bb2e744bd89555b22bfadef5b29d
Allen Poblete <allen.poblete@guerilla360.com>
2024-09-10 17:26:54 +0800

    feat: add config format for ses

--- src/configs/smtp.js
+++ src/configs/smtp.js
@@ -1,17 +1,10 @@
 export default {
-    standard: {
-        host: process.env.SMTP_HOST,
-        port: process.env.SMTP_PORT,
-        secure: process.env.SMTP_SECURE === 'true',
-        user: process.env.SMTP_USER,
-        pass: process.env.SMTP_PASS,
+    ses: {
+        apiVersion: '2010-12-01',
+        region: process.env.SMTP_REGION,
+        credentials: {
+            accessKeyId: process.env.SMTP_USER,
+            secretAccessKey: process.env.SMTP_PASSWORD,
+        },
     },
-    // ses: {
-    //     apiVersion: '2010-12-01',
-    //     region: process.env.SMTP_REGION,
-    //     credentials: {
-    //         accessKeyId: process.env.SMTP_USER,
-    //         secretAccessKey: process.env.SMTP_PASSWORD,
-    //     },
-    // },
 };
