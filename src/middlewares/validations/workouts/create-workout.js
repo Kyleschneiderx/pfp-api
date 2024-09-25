5cbe3bc63e5e3a8ef03a79bff025014f6b7063b0
Allen Poblete <allen.poblete@guerilla360.com>
2024-09-25 17:24:13 +0800

    fix: arrangement of validation

--- src/middlewares/validations/workouts/create-workout.js
+++ src/middlewares/validations/workouts/create-workout.js
@@ -9,7 +9,6 @@ export default ({ exerciseService, selectionService, file }) => [
     ...commonValidation.photoValidation({ field: 'photo', file: file, isRequired: true }),
     body('is_premium').trim().exists({ value: 'falsy' }).isBoolean(),
     body('exercises')
-        .if(body('status_id').equals(String(PUBLISHED_WORKOUT_STATUS_ID)))
         .customSanitizer((value) => {
             if (value === '') return undefined;
 
@@ -21,6 +20,8 @@ export default ({ exerciseService, selectionService, file }) => [
 
             return value;
         })
+        .if(body('status_id').equals(String(PUBLISHED_WORKOUT_STATUS_ID)))
+
         .exists({ value: 'falsy' }),
     commonValidation.exerciseIdValidation({ exerciseService, isBody: true, field: 'exercises.*.exercise_id' }),
     body('exercises.*.sets')
