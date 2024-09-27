15588cb2d9a9195f4ee8c8c9bfbf55ef5a5e94be
Allen Poblete <allen.poblete@guerilla360.com>
2024-09-27 16:53:34 +0800

    refactor: remove unnecessary process

--- src/middlewares/validations/common/education-id.js
+++ src/middlewares/validations/common/education-id.js
@@ -15,10 +15,6 @@ export default ({ educationService, field = 'id', isBody = false, isRequired = t
     rule.withMessage('Education id is required.')
         .customSanitizer((value) => Number(value))
         .custom(async (value, { req }) => {
-            if (!(await educationService.isEducationExistById(value))) {
-                throw new Error('Education does not exist.');
-            }
-
             const isEducationExist =
                 req.auth.account_type_id === ADMIN_ACCOUNT_TYPE_ID
                     ? await educationService.isEducationExistById(value)
