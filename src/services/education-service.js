5a381bff91ff50ff709a2c89eed45fcca676092e
Allen Poblete <allen.poblete@guerilla360.com>
2024-10-17 13:30:47 +0800

    feat: add reference pf plan for education

--- src/services/education-service.js
+++ src/services/education-service.js
@@ -357,6 +357,7 @@ export default class EducationService {
                     'description',
                     'content',
                     'photo',
+                    'reference_pf_plan_id',
                     'media_url',
                     'media_upload',
                     ...(filter?.authenticatedUser?.account_type_id !== ADMIN_ACCOUNT_TYPE_ID
