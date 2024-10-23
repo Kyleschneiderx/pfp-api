238b5d673d35fdf21fc7c61cc0eee55df47f3ab7
Allen Poblete <allen.poblete@guerilla360.com>
2024-10-23 11:06:46 +0800

    refactor: add function to verify sso token

--- src/middlewares/validations/auth/login.js
+++ src/middlewares/validations/auth/login.js
@@ -1,106 +1,113 @@
 import { body } from 'express-validator';
 import * as constants from '../../../constants/index.js';
 
-export default ({ userService, password, isAdmin, authService }) => [
-    body('email')
-        .trim()
-        .if(body('google_token').not().exists({ values: 'falsy' }))
-        .if(body('apple_token').not().exists({ values: 'falsy' }))
-        .exists({ values: 'falsy' })
-        .withMessage('Email is required.')
-        .isString()
-        .isEmail()
-        .isLength({ max: 150 })
-        .customSanitizer(async (value, { req }) => {
-            req.user = await userService.getUser({
-                email: value,
-                accountTypeId: isAdmin ? constants.ADMIN_ACCOUNT_TYPE_ID : constants.USER_ACCOUNT_TYPE_ID,
-                withProfile: true,
-            });
+export default ({ userService, password, isAdmin, authService }) => {
+    const verifySsoToken = async (token, ssoIdentifier) => {
+        let decodedToken;
+        try {
+            decodedToken = await authService.verifySocialMediaIdToken(token);
+        } catch (error) {
+            throw new Error('Invalid token.');
+        }
 
-            return value;
-        })
-        .custom(async (value, { req }) => {
-            if (!req.user) {
-                throw new Error('Incorrect email or password.');
-            }
+        if (decodedToken?.firebase?.identities?.[ssoIdentifier] === undefined) {
+            throw new Error('Invalid token.');
+        }
 
-            return true;
-        }),
-    body('password')
-        .trim()
-        .if(body('google_token').not().exists({ values: 'falsy' }))
-        .if(body('apple_token').not().exists({ values: 'falsy' }))
-        .exists({ values: 'falsy' })
-        .withMessage('Password is required.')
-        .isString()
-        .custom(async (value, { req }) => {
-            if (!password.verify(value, req.user.password)) {
-                throw new Error('Incorrect email or password.');
-            }
+        return decodedToken.firebase.identities[ssoIdentifier][0];
+    };
 
-            return true;
-        }),
-    body('google_token')
-        .trim()
-        .if(body('google_token').exists({ values: 'falsy' }))
-        .exists({ values: 'falsy' })
-        .withMessage('Google token is required.')
-        .isString()
-        .custom(async (value, { req }) => {
-            let decodedToken;
-            try {
-                decodedToken = await authService.verifySocialMediaIdToken(value);
-            } catch (error) {
-                throw new Error('Invalid google token.');
-            }
+    return [
+        body('email')
+            .trim()
+            .if(body('google_token').not().exists({ values: 'falsy' }))
+            .if(body('apple_token').not().exists({ values: 'falsy' }))
+            .exists({ values: 'falsy' })
+            .withMessage('Email is required.')
+            .isString()
+            .isEmail()
+            .isLength({ max: 150 })
+            .customSanitizer(async (value, { req }) => {
+                req.user = await userService.getUser({
+                    email: value,
+                    accountTypeId: isAdmin ? constants.ADMIN_ACCOUNT_TYPE_ID : constants.USER_ACCOUNT_TYPE_ID,
+                    withProfile: true,
+                });
 
-            if (decodedToken?.firebase?.identities?.['google.com'] === undefined) {
-                throw new Error('Invalid google token.');
-            }
+                return value;
+            })
+            .custom(async (value, { req }) => {
+                if (!req.user) {
+                    throw new Error('Incorrect email or password.');
+                }
 
-            const googleId = decodedToken.firebase.identities['google.com'][0];
+                return true;
+            }),
+        body('password')
+            .trim()
+            .if(body('google_token').not().exists({ values: 'falsy' }))
+            .if(body('apple_token').not().exists({ values: 'falsy' }))
+            .exists({ values: 'falsy' })
+            .withMessage('Password is required.')
+            .isString()
+            .custom(async (value, { req }) => {
+                if (!password.verify(value, req.user.password)) {
+                    throw new Error('Incorrect email or password.');
+                }
 
-            req.user = await userService.getUser({
-                googleId: googleId,
-                accountTypeId: constants.USER_ACCOUNT_TYPE_ID,
-                withProfile: true,
-            });
-            if (!req.user) {
-                throw new Error('Account does not exist.');
-            }
+                return true;
+            }),
+        body('google_token')
+            .trim()
+            .if(body('google_token').exists({ values: 'falsy' }))
+            .exists({ values: 'falsy' })
+            .withMessage('Google token is required.')
+            .isString()
+            .custom(async (value, { req }) => {
+                let googleId;
 
-            return true;
-        }),
-    body('apple_token')
-        .trim()
-        .if(body('apple_token').exists({ values: 'falsy' }))
-        .exists({ values: 'falsy' })
-        .withMessage('Apple token is required.')
-        .isString()
-        .custom(async (value, { req }) => {
-            let decodedToken;
-            try {
-                decodedToken = await authService.verifySocialMediaIdToken(value);
-            } catch (error) {
-                throw new Error('Invalid apple token.');
-            }
+                try {
+                    googleId = await verifySsoToken(value, 'google.com');
+                } catch (error) {
+                    throw new Error('Invalid google token.');
+                }
 
-            if (decodedToken?.firebase?.identities?.['apple.com'] === undefined) {
-                throw new Error('Invalid apple token.');
-            }
+                req.user = await userService.getUser({
+                    googleId: googleId,
+                    accountTypeId: constants.USER_ACCOUNT_TYPE_ID,
+                    withProfile: true,
+                });
+                if (!req.user) {
+                    throw new Error('Account does not exist.');
+                }
 
-            const appleId = decodedToken.firebase.identities['apple.com'][0];
+                return true;
+            }),
+        body('apple_token')
+            .trim()
+            .if(body('apple_token').exists({ values: 'falsy' }))
+            .exists({ values: 'falsy' })
+            .withMessage('Apple token is required.')
+            .isString()
+            .custom(async (value, { req }) => {
+                let appleId;
 
-            req.user = await userService.getUser({
-                appleId: appleId,
-                accountTypeId: constants.USER_ACCOUNT_TYPE_ID,
-                withProfile: true,
-            });
-            if (!req.user) {
-                throw new Error('Account does not exist.');
-            }
+                try {
+                    appleId = await verifySsoToken(value, 'apple.com');
+                } catch (error) {
+                    throw new Error('Invalid apple token.');
+                }
 
-            return true;
-        }),
-];
+                req.user = await userService.getUser({
+                    appleId: appleId,
+                    accountTypeId: constants.USER_ACCOUNT_TYPE_ID,
+                    withProfile: true,
+                });
+                if (!req.user) {
+                    throw new Error('Account does not exist.');
+                }
+
+                return true;
+            }),
+    ];
+};
