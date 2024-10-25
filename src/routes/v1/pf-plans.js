63a7c2c5321d08bd8884ac29f1a7b1e6d1a98385
Allen Poblete <allen.poblete@guerilla360.com>
2024-10-25 15:11:06 +0800

    feat: add validation for user only endpoints

--- src/routes/v1/pf-plans.js
+++ src/routes/v1/pf-plans.js
@@ -3,44 +3,44 @@ import validateInput from '../../middlewares/validate-input.js';
 import * as validations from '../../middlewares/validations/pf-plans/index.js';
 import * as commonValidations from '../../middlewares/validations/common/index.js';
 
-export default ({ verifyAdmin, pfPlanController, pfPlanService, exerciseService, selectionService, file, educationService }) => {
+export default ({ verifyAdmin, verifyUser, pfPlanController, pfPlanService, exerciseService, selectionService, file, educationService }) => {
     const router = express.Router();
 
     router.get('/', validateInput(validations.getPfPlansValidation()), pfPlanController.handleGetPfPlansRoute.bind(pfPlanController));
 
     router.get(
         '/favorites',
-        validateInput(validations.getFavoritePfPlansValidation()),
+        [validateInput(validations.getFavoritePfPlansValidation()), verifyUser],
         pfPlanController.handleGetFavoritePfPlansRoute.bind(pfPlanController),
     );
 
     router.post(
         '/:id/favorite',
-        validateInput([validations.updateFavoritePfPlanValidation({ pfPlanService, isFavorite: true })]),
+        [validateInput([validations.updateFavoritePfPlanValidation({ pfPlanService, isFavorite: true })]), verifyUser],
         pfPlanController.handleAddFavoritePfPlanRoute.bind(pfPlanController),
     );
 
     router.delete(
         '/:id/favorite',
-        validateInput([validations.updateFavoritePfPlanValidation({ pfPlanService, isUnfavorite: true })]),
+        [validateInput([validations.updateFavoritePfPlanValidation({ pfPlanService, isUnfavorite: true })]), verifyUser],
         pfPlanController.handleRemoveFavoritePfPlanRoute.bind(pfPlanController),
     );
 
     router.put(
         '/:id/select',
-        validateInput(validations.selectPfPlanValidation({ pfPlanService })),
+        [validateInput(validations.selectPfPlanValidation({ pfPlanService })), verifyUser],
         pfPlanController.handleSelectPfPlanRoute.bind(pfPlanController),
     );
 
     router.delete(
         '/:id/deselect',
-        validateInput(validations.deselectPfPlanValidation({ pfPlanService })),
+        [validateInput(validations.deselectPfPlanValidation({ pfPlanService })), verifyUser],
         pfPlanController.handleDeselectPfPlanRoute.bind(pfPlanController),
     );
 
     router.put(
         '/:id/progress',
-        validateInput(validations.updatePfPlanProgressValidation({ pfPlanService, educationService, exerciseService })),
+        [validateInput(validations.updatePfPlanProgressValidation({ pfPlanService, educationService, exerciseService })), verifyUser],
         pfPlanController.handleUpdatePfPlanProgressRoute.bind(pfPlanController),
     );
 
