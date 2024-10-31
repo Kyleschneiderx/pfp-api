bbfb3f4c7c9bdab58f856e5aa9144cc4e714c0ae
Allen Poblete <allen.poblete@guerilla360.com>
2024-10-31 16:11:06 +0800

    test: update scheduled task to per midnight

--- src/tasks/scan-pf-plan-retention-period.js
+++ src/tasks/scan-pf-plan-retention-period.js
@@ -3,7 +3,7 @@ export default ({ logger, pfPlanService }) => {
 
     return {
         name: name,
-        schedule: '* * * * *',
+        schedule: '0 0 * * *',
         process: async () => {
             logger.info(`Starting task [${name}]`);
             try {
