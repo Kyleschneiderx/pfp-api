c9e2fe0bb56165ff07944ed77cc7293670ba9c35
Allen Poblete <allen.poblete@guerilla360.com>
2024-11-06 14:00:58 +0800

    chore: temporary disable task

--- src/tasks/index.js
+++ src/tasks/index.js
@@ -3,7 +3,7 @@ import scanPfPlanRetentionPeriod from './scan-pf-plan-retention-period.js';
 import dailyPfPlanReminder from './daily-pf-plan-reminder.js';
 
 export default ({ logger, userService, pfPlanService, notificationService } = {}) => [
-    deactivateInactiveAccounts({ logger, userService, notificationService }),
-    scanPfPlanRetentionPeriod({ logger, pfPlanService }),
-    dailyPfPlanReminder({ logger, pfPlanService, notificationService }),
+    // deactivateInactiveAccounts({ logger, userService, notificationService }),
+    // scanPfPlanRetentionPeriod({ logger, pfPlanService }),
+    // dailyPfPlanReminder({ logger, pfPlanService, notificationService }),
 ];
