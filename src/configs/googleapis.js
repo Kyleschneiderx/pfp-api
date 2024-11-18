781256c7418280185c0c999484b0d365e3c83db6
Allen Poblete <allen.poblete@guerilla360.com>
2024-11-18 16:08:08 +0800

    debug: issue with in-app credential and server env

--- src/configs/googleapis.js
+++ src/configs/googleapis.js
@@ -1,4 +1,4 @@
 export default {
-    clientEmail: process.env.GOOGLE_PAY_CLIENT_EMAIL,
-    privateKey: process.env.GOOGLE_PAY_SECRET.replace(/\\n/g, '\n'),
+    // clientEmail: process.env.GOOGLE_PAY_CLIENT_EMAIL,
+    // privateKey: process.env.GOOGLE_PAY_SECRET.replace(/\\n/g, '\n'),
 };
