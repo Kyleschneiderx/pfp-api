72c662c609e35b982fba0b034520ad09e4be8b09
Allen Poblete <allen.poblete@guerilla360.com>
2025-02-21 17:54:03 +0800

    fix: make rating reason optional and rating required when rating reason is not empty

--- src/middlewares/validations/miscellaneous/send-feedback.js
+++ src/middlewares/validations/miscellaneous/send-feedback.js
@@ -2,34 +2,34 @@ import { body, oneOf } from 'express-validator';
 
 export default () => [
     oneOf([
-        [
-            body('rating')
-                .exists({ values: 'falsy' })
-                .withMessage('Rating is required.')
-                .isNumeric()
-                .withMessage('Rating should be number.')
-                .isInt({ min: 1, max: 10 })
-                .withMessage('Rating should be between 1 and 10.'),
-            body('rating_reason')
-                .trim()
-                .exists({ values: 'falsy' })
-                .withMessage('Rating reason is required.')
-                .isString()
-                .withMessage('Rating reason should be string.')
-                .exists({ value: 'falsy' })
-                .withMessage('Rating reason is required.'),
-        ],
-        body('useful_feature')
-            .isString()
-            .withMessage('Useful feature should be string')
-            .exists({ value: 'falsy' })
-            .withMessage('Useful feature is required.')
-            .trim(),
-        body('enhancement')
-            .isString()
-            .withMessage('Enhancement should be string.')
-            .exists({ value: 'falsy' })
-            .withMessage('Enhancement is required.')
-            .trim(),
+        body('rating').exists({ value: 'falsy' }).withMessage('Rating is required.'),
+        body('rating_reason').exists({ value: 'falsy' }).withMessage('Rating reason is required.'),
+        body('useful_feature').exists({ value: 'falsy' }).withMessage('Useful feature is required.'),
+        body('enhancement').exists({ value: 'falsy' }).withMessage('Enhancement is required.'),
     ]),
+    body('rating')
+        .if(body('rating_reason').exists({ value: 'falsy' }))
+        .if(oneOf([body('rating').not().exists({ value: 'falsy' }), body('rating_reasons').exists({ value: 'falsy' })]))
+        // .custom((value) => console.log(123))
+        .exists({ value: 'falsy' })
+        .withMessage('Rating is required.')
+        .isNumeric()
+        .withMessage('Rating should be number.')
+        .isInt({ min: 1, max: 10 })
+        .withMessage('Rating should be between 1 and 10.'),
+    body('rating_reason')
+        .if(body('rating_reason').exists({ value: 'falsy' }))
+        .isString()
+        .withMessage('Rating reason should be string.')
+        .trim(),
+    body('useful_feature')
+        .if(body('useful_feature').exists({ value: 'falsy' }))
+        .isString()
+        .withMessage('Useful feature should be string')
+        .trim(),
+    body('enhancement')
+        .if(body('enhancement').exists({ value: 'falsy' }))
+        .isString()
+        .withMessage('Enhancement should be string.')
+        .trim(),
 ];
