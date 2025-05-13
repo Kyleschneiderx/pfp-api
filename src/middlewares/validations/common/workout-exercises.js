4244de35039ee5d1b33090ff95042fb536d2faaa
Allen Poblete <allen.poblete@guerilla360.com>
2025-05-13 11:23:22 +0800

    feat: add rest on workout exercises

--- src/middlewares/validations/common/workout-exercises.js
+++ src/middlewares/validations/common/workout-exercises.js
@@ -22,12 +22,17 @@ export default ({ exerciseService }) => [
         .isInt({ gt: 0 })
         .withMessage('Reps should be greater than 0.'),
     body('exercises.*.hold')
+        .optional()
+        .customSanitizer((value) => Number(value))
+        .isNumeric()
+        .withMessage('Hold should be numeric'),
+    body('exercises.*.rest')
         .trim()
         .exists({ values: 'falsy' })
         .withMessage('Number of hold is required.')
         .customSanitizer((value) => Number(value))
         .isNumeric()
-        .withMessage('Hold should be numeric')
+        .withMessage('Rest should be numeric')
         .isInt({ gt: 0 })
-        .withMessage('Hold should be greater than 0.'),
+        .withMessage('Rest should be greater than 0.'),
 ];
