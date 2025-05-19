c32383bb196803e3bbaa3a07ffbf60a9d652dd17
Allen Poblete <allen.poblete@guerilla360.com>
2025-05-19 21:07:36 +0800

    chore: remove endpoint for convert image instead use the task queue

--- src/routes/api.js
+++ src/routes/api.js
@@ -157,7 +157,7 @@ export default ({
 
     router.use(verifyAuth);
 
-    router.use('/scripts', routeScripts({ verifyAdmin: middlewares.verifyAdmin, database, file, storage, helper }));
+    router.use('/scripts', routeScripts({ verifyAdmin: middlewares.verifyAdmin, database, helper }));
 
     router.use(
         '/v1/selections',
