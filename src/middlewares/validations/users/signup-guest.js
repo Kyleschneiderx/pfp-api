4dec36dce6d931d5b23bc9b286d8ead6e522b98a
Allen Poblete <allen.poblete@guerilla360.com>
2025-06-11 10:00:47 +0800

    fix: refactor and add device id validation

--- src/middlewares/validations/users/signup-guest.js
+++ src/middlewares/validations/users/signup-guest.js
@@ -1,3 +1,8 @@
+import { body } from 'express-validator';
 import * as commonValidation from '../common/index.js';
 
-export default ({ file }) => [...commonValidation.createGuestValidation({ file: file })];
+export default ({ file }) => [
+    body('device_id').trim().exists({ values: 'falsy' }).withMessage('Device id is required.').isString().isLength({ max: 150 }),
+    body('name').trim().exists({ values: 'falsy' }).withMessage('Name is required.').isString().isLength({ max: 150 }),
+    ...commonValidation.photoValidation({ field: 'photo', file: file }),
+];
