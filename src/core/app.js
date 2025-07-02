53f34d543c389dcba54ea3f0aecaf74bed2c68b7
Allen Poblete <allen.poblete@guerilla360.com>
2025-07-02 11:42:29 +0800

    feat: add script for migrating user to firestore and create initial room

--- src/core/app.js
+++ src/core/app.js
@@ -96,6 +96,8 @@ export default ({ serviceContainer }) => {
             inAppPurchase: serviceContainer.inAppPurchase,
             storage: serviceContainer.storage,
             revenuecat: serviceContainer.revenuecat,
+            fireStore: serviceContainer.fireStore,
+            database: serviceContainer.database,
         }),
     );
 
